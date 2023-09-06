9c15a76f5e9ce0f312bf608c1f81236de383a262
djay <djaydegier@gmail.com>
2023-09-06 10:14:10 +0200

    About me beter

--- projecten.php
+++ projecten.php
@@ -57,7 +57,7 @@
             $ret = $db->query($sql);
             while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
                 echo "<project onclick=\"window.open('{$row['url']}', '_blank')\">";
-                echo "<img src='media/{$row['image']}'>";
+                echo "<img src='media/logos/{$row['image']}'>";
                 echo "<div class='title'>{$row['title']}</div>";
                 echo "<div class='desc'>{$row['desc']}</div>";
                 echo "</project>";
